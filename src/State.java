301f3dda94e872665610335e3374a25fa3ea0df1
aliFuatOzturk <fuat.ozturk@ozu.edu.tr>
2022-11-06 22:47:07 +0300

    basic work

---
+++ src/State.java
@@ -0,0 +1,10 @@
+public class State {
+    private String state;
+    public State (String nameOfState){this.state =nameOfState;}
+    public String getState(){
+        return this.state;
+    }
+    public void setState(String N){
+        this.state =N;
+    }
+}
