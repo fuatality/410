fd34b3baf5ad7f898ba557876241f9a3c12f7929
aliFuatOzturk <fuat.ozturk@ozu.edu.tr>
2022-12-11 12:03:24 +0300

    last commit proj1

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class Main
 {
+
     public static void main(String[] args) throws Exception
     {
-        readFile();
+        Table table=new Table();
+        ReadFile readFile=new ReadFile();
+        readFile.readFile();
+        table.constructTable(readFile.getTransition(),readFile.getAlphabet(),readFile.getState());
+
     }
-    public static void readFile() throws FileNotFoundException {
-        // pass the path to the file as a parameter
-        File file = new File("C:\\Users\\fuatw\\IdeaProjects\\NfaToDfa\\src\\NFA1.txt");
-        Scanner sc = new Scanner(file);
-        ArrayList<String> nfaFile=new ArrayList<>();
-        while (sc.hasNextLine()){
-            nfaFile.add(sc.nextLine());
-        }
-        int statePos= nfaFile.indexOf("STATES");
-        int startPos= nfaFile.indexOf("START");
-        int finalPos= nfaFile.indexOf("FINAL");
-        int transPos= nfaFile.indexOf("TRANSITIONS");
-        int endPos  = nfaFile.indexOf("END");
-        ArrayList<String> alphabet = new ArrayList<>(nfaFile.subList(1,statePos));
-        System.out.println("Alphabet: "+alphabet);
-        ArrayList<String> states = new ArrayList<>(nfaFile.subList(statePos+1,startPos));
-        System.out.println("States: "+states);
-        StartState startState=new StartState(nfaFile.get(startPos+1));
-        startState.print();
-        FinalState finalState=new FinalState(nfaFile.get(finalPos+1));
-        finalState.print();
-        ArrayList<String> transitions=new ArrayList<>(nfaFile.subList(transPos+1,endPos));
-        System.out.println("Transitions: "+transitions);
-    }
+
 }
--- src/State.java
+++ src/State.java
@@ -1,10 +1,20 @@
+import java.util.ArrayList;
+
 public class State {
-    private String state;
-    public State (String nameOfState){this.state =nameOfState;}
-    public String getState(){
-        return this.state;
+    private ArrayList<String> states;
+    public State (ArrayList<String> nameOfState){this.states =nameOfState;}
+
+    public State() {
+
     }
-    public void setState(String N){
-        this.state =N;
+
+    public ArrayList<String> getState(){
+        return this.states;
+    }
+    public int getStateCount(){
+        return this.states.size();
+    }
+    public void setState(ArrayList<String> N){
+        this.states =N;
     }
 }
